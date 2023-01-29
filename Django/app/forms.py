0a9ee354f9de2c077f3281d39e45c2569abcdf1d
aramdan <abdodo59@gmail.com>
2023-01-30 00:59:14 +0100

    update formulaire

--- Django/app/forms.py
+++ Django/app/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 
 class ApiForm(forms.Form):
-    age = forms.IntegerField(initial=18)
-    sex = forms.CharField(initial='male')
-    children = forms.IntegerField(initial=4)
-    smoker = forms.CharField(initial='yes')
-    region = forms.CharField(initial='northeast')
-    grade = forms.CharField(initial='obesity')
+    Naics = forms.IntegerField()
+    UrbanRural = forms.CharField()
+    NoEmp = forms.IntegerField(initial=4)
+    RetainedJob = forms.CharField()
+    Term = forms.CharField()
--- Django/app/views.py
+++ Django/app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .forms import ApiForm
+import requests
+import json
 
-
+# Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+    # header = {'accept': 'application/json',
+    # 'Content-Type': 'application/json'}
+
+    form = ApiForm(request.POST or None)
+    if request.method == 'POST':
+        form = ApiForm(request.POST)
+        if form.is_valid():
+            data = json.dumps(form.cleaned_data)
+            reponse = requests.post('http://127.0.0.1:8000/predict', data=data)
+            info = reponse.text
+            return render(request, 'app/home.html', context={'form' : form, 'info' : info} )
+
+    context = {'form' : form}
+    return render(request, 'app/home.html', context=context )
